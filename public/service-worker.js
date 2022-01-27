009f54db72dbc0e1732de3ca3e15166258a369a6
Tracy Torisky <mommers1@verizon.net>
2022-01-27 18:21:47 -0500

    created the logic to cache responses for requests

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,12 @@
 const STATIC_CACHE = "static-cache-v1"
+const RUNTIME_CACHE = "runtime-cache"
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
     "/index.js",
     "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
 ]
 
 self.addEventListener("install", (event) => {
@@ -16,3 +19,64 @@ self.addEventListener("install", (event) => {
             })
     )
 })
+
+self.addEventListener("activate", event => {
+    const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+    event.waitUntil(
+        caches
+            .keys()
+            .then(cacheNames => {
+                // return array of cache names that are old to delete
+                return cacheNames.filter(
+                    cacheName => !currentCaches.includes(cacheName)
+                );
+            })
+            .then(cachesToDelete => {
+                return Promise.all(
+                    cachesToDelete.map(cacheToDelete => {
+                        return caches.delete(cacheToDelete);
+                    })
+                );
+            })
+            .then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    if (event.request.method !== "GET" || !event.request.url.startsWith(self.location.origin)) {
+        event.respondWith(fetch(event.request))
+        return
+    }
+
+    if (event.request.url.includes("/api/transaction")) {
+        event.respondWith(
+            caches.open(RUNTIME_CACHE).then((cache) => {
+                return fetch(event.request)
+                    .then((response) => {
+                        cache.put(event.request, response.clone())
+                        return response
+                    })
+                    .catch(() => {
+                        return caches.match(event.request)
+                    })
+            })
+        )
+        return
+    }
+
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            if (cachedResponse) {
+                return cachedResponse
+            }
+
+            return caches.open(RUNTIME_CACHE).then((cache)=> {
+                return fetch(event.request).then((response)=> {
+                    return cache.put(event.request, response.clone()).then(() => {
+                        return response
+                    })
+                })
+            })
+        })
+    )
+}) 
