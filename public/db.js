60c849face46fe670b8606d134028e4f820a25ac
Tracy Torisky <mommers1@verizon.net>
2022-01-27 18:54:46 -0500

    added check database logic

---
+++ public/db.js
@@ -0,0 +1,55 @@
+const indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB
+
+let db
+
+const request = indexedDB.open("budget", 1)
+
+request.onupgradeneeded = (event) => {
+    const db = event.target.result
+    db.createObjectStore("pending", {
+        autoIncrement: true
+    })
+}
+
+request.onsuccess = (event) => {
+    db = event.target.result
+    if (navigator.onLine) {
+        checkDatabase()
+    }
+}
+
+request.onerror = (event) => {
+    console.log("Whoops! " + event.target.errorCode)
+}
+
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite")
+    const store = transaction.objectStore("pending")
+    store.add(record)
+}
+
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite")
+    const store = transaction.objectStore("pending")
+    const getAll = store.getAll()
+    getAll.onsuccess = () => {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+                .then((response) => {
+                    return response.json()
+                })
+                .then(() => {
+                    const transaction = db.transaction(["pending"], "readwrite")
+                    const store = transaction.objectStore("pending")
+                    store.clear()
+                })
+        }
+    }
+}
